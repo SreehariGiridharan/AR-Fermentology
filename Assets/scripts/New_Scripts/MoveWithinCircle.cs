0db522d47b9fb53c862e58ad37052b9bd91e4032
SreehariGiridharan <sreehariak777@gmail.com>
2024-05-28 23:48:51 +0200

    Feature Update: Barcode Added, resized the water, set everything working

--- Assets/scripts/New_Scripts/MoveWithinCircle.cs
+++ Assets/scripts/New_Scripts/MoveWithinCircle.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MoveWithinCircle : MonoBehaviour
 {
-    public float moveSpeed = 1.0f; // Speed of movement
+    private float moveSpeed = 0.01f; // Speed of movement
     
     public float moveDuration = 5.0f; // Duration of movement in seconds
     private float elapsedTime = 0.0f; // Time elapsed since movement started
