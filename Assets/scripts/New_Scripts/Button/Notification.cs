7b87370ff6413aa28ab643735b305de1feb46ef5
SreehariGiridharan <sreehariak777@gmail.com>
2024-06-11 22:38:55 +0200

    Feature Update: Changed size of H20,Co2. Added H20 notification bar

---
+++ Assets/scripts/New_Scripts/Button/Notification.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections; // Add this line to import the IEnumerator interface
+
+public class Notification : MonoBehaviour
+{
+    public Animator animator; // Reference to the Animator component
+    public string firstAnimationName; // Name of the first animation to play
+    public string secondAnimationName; // Name of the second animation to play
+    public float delayBetweenAnimations = 2f; // Time in seconds to wait between animations
+
+    private void Start()
+    {
+        // Start the coroutine to play animations sequentially
+        StartCoroutine(PlayAnimations());
+    }
+
+    private IEnumerator PlayAnimations()
+    {
+        // Play the first animation
+        animator.Play(firstAnimationName);
+
+        // Wait for the specified delay
+        yield return new WaitForSeconds(delayBetweenAnimations);
+
+        // Play the second animation
+        animator.Play(secondAnimationName);
+    }
+}
